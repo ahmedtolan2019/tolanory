1e170ee6e98cef8ddbd36c7e8538dca456909b3d
ahmedtolan2019 <Ahmed.Tolan@sh-eng.menofia.edu.eg>
2020-12-19 19:16:03 +0200

    some other small changes to work with heroku

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -16,7 +16,7 @@ passport.deserializeUser((id, done) => {
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/callback"
+    callbackURL: process.env.CALLBACK_URL
   }, (accessToken, refreshToken, profile, done) => {
     //handle cb function
    
@@ -26,8 +26,7 @@ passport.use(new GoogleStrategy({
       //if there is the user in database i.e googleid = profile.id
       //then call done method to convert to cookie
       if (currentUser) {
-        console.log(profile)
-        console.log(currentUser.image)
+       
         
         done(null, currentUser)
       } else {
